5de712e010610c97d162b8cfde94f09d97d7cab5
Osvaldbp <osvald.johnsson@gmail.com>
2025-10-16 10:30:50 +0200

    added a loop for the user so that exit is more "useful" fixed println stuff

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -11,63 +11,76 @@ public class Main {
     final static double EURO_TO_SEK = 11.05;
 
     public static void main(String[] args) {
-
-
+        Scanner userInput = new Scanner(System.in);
 
         System.out.println("Currency Converter App:");
 
-        String[] convertOptions = {
-                "Convert SEK to USD",
-                "Convert USD to SEK",
-                "Convert SEK to Euro",
-                "Convert euro to SEK",
-                "Exit"
-        };
+        while (true) {
+            String[] convertOptions = {
+                    "Convert SEK to USD",
+                    "Convert USD to SEK",
+                    "Convert SEK to Euro",
+                    "Convert euro to SEK",
+                    "Exit"
+            };
 
-        for (int i = 0; i < convertOptions.length; i++) {
-            System.out.println((i + 1) + ". " + convertOptions[i]);
-        }
-        Scanner userInput = new Scanner(System.in);
-        int choice = userInput.nextInt();
+            for (int i = 0; i < convertOptions.length; i++) {
+                System.out.println((i + 1) + ". " + convertOptions[i]);
+            }
+            System.out.print("Enter your choice: ");
+            int choice = userInput.nextInt();
 
-        System.out.println("Enter your choice: ");
-        double amount = userInput.nextDouble();
+            double result = 0;
+            double amount = 0;
 
-        double result = 0;
+            if (choice == 5) {
+                System.out.println("Closing program...");
+                break;
+            }
 
-        switch (choice) {
-            case 1:
-                result = convertSEKToUSD(amount);
-                System.out.println(amount + " converted to USD");
-                break;
-            case 2:
-                result = convertUSDToSEK(amount);
-                System.out.println(amount + " converted to SEK");
-                break;
-            case 3:
-                result = convertSEKToEuro(amount);
-                System.out.println(amount + " converted to Euro");
-                break;
-            case 4:
-                result = convertEuroToSEK(amount);
-                System.out.println(amount + " converted to SEK");
-                break;
+
+            System.out.print("Enter your amount: ");
+            amount = userInput.nextDouble();
+
+
+            switch (choice) {
+                case 1:
+                    result = convertSEKToUSD(amount);
+                    System.out.println(amount + " SEK =" + result + " USD");
+                    break;
+                case 2:
+                    result = convertUSDToSEK(amount);
+                    System.out.println(amount + " USD =" + result + " SEK");
+                    break;
+                case 3:
+                    result = convertSEKToEuro(amount);
+                    System.out.println(amount + " USD =" + result + " SEK");
+                    break;
+                case 4:
+                    result = convertEuroToSEK(amount);
+                    System.out.println(amount + " USD =" + result + " SEK");
+                    break;
                 default:
                     System.out.println("Invalid choice");
 
+            }
         }
 
-       userInput.close();
+        userInput.close();
     }
+
     public static double convertSEKToUSD(double amount) {
         return amount * SEK_TO_USD;
     }
+
     public static double convertUSDToSEK(double amount) {
         return amount * USD_TO_SEK;
     }
+
     public static double convertSEKToEuro(double amount) {
         return amount * SEK_TO_EURO;
     }
+
     public static double convertEuroToSEK(double amount) {
         return amount * EURO_TO_SEK;
     }
