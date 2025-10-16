d2d855da8b2821dd855dbe9d4a0ae06fad152afc
Osvaldbp <osvald.johnsson@gmail.com>
2025-10-16 09:18:10 +0200

    First commit

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,7 +2,17 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Currency Converter App:");
+        String[] convertOptions = {
+                "Convert SEK to USD"
+                "Convert USD to SEK"
+                "Convert SEK to Euro"
+                "Convert euro to SEK"
+                "Exit"
+        }
 
-        while {
+        while (true) {
+
+        }
     }
 }
