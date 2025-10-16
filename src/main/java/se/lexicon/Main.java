906e0a2ab88e6b68a4a069a7b3bec174ad6b4ba9
Osvaldbp <osvald.johnsson@gmail.com>
2025-10-16 09:26:27 +0200

    third commit (last was not first)

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,18 +1,25 @@
 package se.lexicon;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Currency Converter App:");
+
         String[] convertOptions = {
-                "Convert SEK to USD"
-                "Convert USD to SEK"
-                "Convert SEK to Euro"
-                "Convert euro to SEK"
+                "Convert SEK to USD",
+                "Convert USD to SEK",
+                "Convert SEK to Euro",
+                "Convert euro to SEK",
                 "Exit"
-        }
+        };
 
-        while (true) {
-
+        for (int i = 0; i < convertOptions.length; i++) {
+            System.out.println((i + 1) + ". " + convertOptions[i]);
         }
+        Scanner userInput = new Scanner(System.in);
+        int choice = userInput.nextInt();
+
     }
+
 }
