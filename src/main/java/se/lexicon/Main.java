c1695d391e344c7ebfe39eaba0737772c1113675
Osvaldbp <osvald.johnsson@gmail.com>
2025-10-16 09:43:12 +0200

    commit 4

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -20,6 +20,28 @@ public class Main {
         Scanner userInput = new Scanner(System.in);
         int choice = userInput.nextInt();
 
+        System.out.println("Enter your choice: ");
+        double amount = userInput.nextDouble();
+
+        double result = 0;
+
+
+    }
+    public static double convertSEKToUSD(double amount) {
+        double rate = 0.11;
+        return amount * rate;
+    }
+    public static double convertUSDToSEK(double amount) {
+        double rate = 9.48;
+        return amount * rate;
+    }
+    public static double convertSEKToEuro(double amount) {
+        double rate = 0.091;
+        return amount * rate;
+    }
+    public static double convertEuroToSEK(double amount) {
+        double rate = 11.05;
+        return amount * rate;
     }
 
 }
